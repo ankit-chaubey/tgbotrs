1c5373c073d046878afaa1d88cadb808de6d1fb2
Ankit Chaubey <ankitchaubey.dev@gmail.com>
2026-04-10 14:57:02 +0530

    .

--- tgbotrs/src/framework/handlers/conversation.rs
+++ tgbotrs/src/framework/handlers/conversation.rs
@@ -9,11 +9,13 @@
 //! };
 //! use tgbotrs::{CommandHandler, MessageHandler};
 //!
+//! fn any_message(_: &tgbotrs::Message) -> bool { true }
+//!
 //! let handler = ConversationHandler::new(
 //!     vec![Box::new(CommandHandler::new("start", |bot, ctx| Box::pin(async move { Ok(()) })))],
 //!     std::collections::HashMap::from([
 //!         ("ask_name".to_string(), vec![
-//!             Box::new(MessageHandler::new("ask_name_handler", |_| true, |bot, ctx| Box::pin(async move { Ok(()) }))) as Box<dyn tgbotrs::Handler>,
+//!             Box::new(MessageHandler::new("ask_name_handler", any_message, |bot, ctx| Box::pin(async move { Ok(()) }))) as Box<dyn tgbotrs::Handler>,
 //!         ]),
 //!     ]),
 //!     ConversationOpts::default(),
